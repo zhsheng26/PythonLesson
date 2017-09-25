28ba5de87af03aabbb7b7f2f6a3f523e100e5ec9
zhangsheng <zhangsheng@timeface.cn>
2017-09-25 18:28:01 +0800

    json

---
+++ 进程和线程/序列化.py
@@ -0,0 +1,30 @@
+# 变量从内存中变成可存储或传输的过程称之为序列化
+# pickling和unpickling
+import json
+import pickle
+
+# pickle.dumps()方法把任意对象序列化成一个bytes
+
+stu_dict = dict(name='andy', age=12, score=90)
+pick_write = pickle.dumps(stu_dict)
+print(pick_write)
+
+file_pick = open('pickling_obj.txt', 'wb')
+with file_pick as f:
+    pickle.dump(stu_dict, f)
+
+pick_read = open('pickling_obj.txt', 'rb')
+with pick_read as re:
+    pickle_load = pickle.load(re)
+    print(pickle_load)
+    print(type(pickle_load))
+# json序列化
+json_dump = json.dumps(stu_dict)
+print(json_dump)
+pick_write_json = open('pickling_json.txt', 'w')
+with pick_write_json as wj:
+    json.dump(stu_dict, wj)
+    json.dump(json_dump, wj)
+# json反序列化
+json_str = '{"age": 20, "score": 88, "name": "Bob"}'
+print(type(json.loads(json_str)))
---
+++ 进程和线程/操作文件和目录.py
@@ -0,0 +1,14 @@
+import os
+
+current_dir = os.path.abspath('.')
+
+path_join = os.path.join(current_dir, '操作文件和目录.py')
+print(path_join)
+
+path_split = os.path.split(path_join)  # ('I:\\PythonLesson\\进程和线程', '操作文件和目录.py')
+print(path_split[0])
+
+dir_list = [x for x in os.listdir('.') if not os.path.isdir(x)]
+print(dir_list)
+py_file = [x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1] == '.py']
+print(py_file)
