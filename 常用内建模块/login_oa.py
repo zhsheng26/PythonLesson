afc19e8b91996497d37cf70a076b3d34e487c32e
zhangsheng <zhsheng26@gmail.com>
2017-10-17 14:19:40 +0800

    login oa

--- 常用内建模块/login_oa.py
+++ 常用内建模块/login_oa.py
@@ -9,8 +9,6 @@ from threading import Timer
 
 import requests
 
-print('Login to TimeFace oa...')
-
 base_url = 'http://oa.v5time.net/tfoa/'
 
 session = requests.Session()
@@ -26,6 +24,8 @@ NOON = 'noon'
 EVENING = 'evening'
 
 USER_NAME = 'zhangsheng@timeface.cn'
+
+print(USER_NAME + ' \nLogin to TimeFace oa...')
 
 
 def login_user():
@@ -45,28 +45,21 @@ def query_check_state(key):
         return ''
 
 
-def get_token():
+def cache_token():
     token_url = "http://tat.tfdom.com/api/v1/token"
     token_req = requests.get(token_url, params={'email': USER_NAME})
-    return json.loads(token_req.text)['token']
-
-
-def cache_token(now_token):
+    now_token = json.loads(token_req.text)['token']
     cache_url = base_url + 'onlineAttendance/cache'
     cache_resp = session.post(cache_url, data={'token': now_token}, headers=headers)
     print(cache_resp.text)
 
 
 def sign(key):
-    if not login_user():
-        return
     sign_time = query_check_state(key)
     if sign_time:
         print(key + ' 已打卡，打卡时间：%s' % sign_time)
     else:
-        # 未打开，可以打卡了
-        token = get_token()
-        cache_token(token)
+        cache_token()
         sign_url = base_url + 'onlineAttendance/sign'
         if key == MORNING:
             flag = 1
@@ -75,48 +68,78 @@ def sign(key):
         else:
             flag = 3
         sign_res = session.post(sign_url, data={'flag': flag}, headers=headers)
-        print(sign_res.text)
+        if sign_res.status_code == 200:
+            print('==*=====*======*==')
+            query()
 
 
 def get_should_sign_time(hour, minute):
     return datetime(datetime.today().year, datetime.today().month, datetime.today().day, hour, minute).strftime(
-        '%Y-%m-%d %H')
+        '%Y-%m-%d %H:%M')
 
 
 # 登录
 # 检查打卡状态
 # 获取当前时间，判断是否是需要打卡的时间
 # 打卡
-# sign(MORNING)
+
 def start_sign():
+    morning_h = 8
+    morning_m = 10
+
+    noon_h = 12
+    noon_m = 33
+
+    evening_h = 18
+    evening_m = 10
     # 星期天不打卡
-    if datetime.today().weekday() == calendar.SUNDAY:
-        return
-    elif datetime.today().weekday() == calendar.SATURDAY:
-        # 8:30
-        time = datetime.today().time()
-        print(time)
-        # 12:30
+    weekday = datetime.today().weekday()
+    if weekday == calendar.SUNDAY:
         return
     else:
-        current_time = datetime.today().__format__('%Y-%m-%d %H')
-        sign_morning = get_should_sign_time(8, 0)  # 早上
-        sign_noon = get_should_sign_time(13, 0)  # 中午
-        sign_evening = get_should_sign_time(18, 0)  # 晚上
+        current_time = datetime.today().__format__('%Y-%m-%d %H:%M')
+        sign_morning = get_should_sign_time(morning_h, morning_m)  # 早上
+        sign_noon = get_should_sign_time(noon_h, noon_m)  # 中午
+        if weekday == calendar.TUESDAY or weekday == calendar.THURSDAY:
+            evening_h = 21
+            evening_m = 12
+        sign_evening = get_should_sign_time(evening_h, evening_m)  # 晚上
         if current_time == sign_morning:
             sign(MORNING)
         elif current_time == sign_noon:
             sign(NOON)
         elif current_time == sign_evening:
             sign(EVENING)
-        else:
-            print('当前时间：%s\n等待打卡...' % datetime.today().__format__('%Y-%m-%d %H:%M:%S'))
     run_sign()
 
 
 def run_sign():
-    Timer(1 * 60 * 10, start_sign).start()
+    Timer(1 * 30, start_sign).start()
+
+
+def query():
+    state_morning = query_check_state(MORNING)
+    if state_morning:
+        print('早上已打卡：%s' % state_morning)
+    else:
+        print('早上待打卡...')
+        return
+    state_noon = query_check_state(NOON)
+    if state_noon:
+        print('中午已打卡：%s' % state_noon)
+    else:
+        print('中午待打卡...')
+        return
+    state_evening = query_check_state(EVENING)
+    if state_evening:
+        print('晚上已打卡：%s' % state_evening)
+    else:
+        print('晚上待打卡...')
+        return
 
 
 if __name__ == '__main__':
-    start_sign()
+    if login_user():
+        print('登录成功!')
+        query()
+        start_sign()
